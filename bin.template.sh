18ec5f87b9dc9e2e10db3abb9a1d3577168ca1bc
da99 <i-hate-spam-1234567@mailinator.com>
2016-01-08 22:02:52 -0600

    Dev. checkpoint.

--- bin.template.sh
+++ bin.template.sh
@@ -1,10 +1,47 @@
 #!/usr/bin/env bash
 #
 #
+THE_ARGS="$@"
+
+if [[ -z "$@" ]]; then
+  action="watch"
+else
+  action=$1
+  shift
+fi
+
 set -u -e -o pipefail
 
-action=$1
-shift
+watch () {
+  cmd () {
+    if [[ -z "$@" ]]; then
+      path="some.file"
+    else
+      path="$1"
+      shift
+    fi
+  }
+  cmd
+
+  echo -e "\n=== Watching: $files"
+  while read -r CHANGE; do
+    dir=$(echo "$CHANGE" | cut -d' ' -f 1)
+    path="${dir}$(echo "$CHANGE" | cut -d' ' -f 3)"
+    file="$(basename $path)"
+
+    echo -e "\n=== $CHANGE ($path)"
+
+    if [[ "$path" =~ "$0" ]]; then
+      echo "=== Reloading..."
+      break
+    fi
+
+    if [[ "$file" =~ ".some_ext" ]]; then
+      cmd $path
+    fi
+  done < <(inotifywait --quiet --monitor --event close_write some_file "$0") || exit 1
+  $0 watch $THE_ARGS
+}
 
 case $action in
   help|--help)
@@ -12,8 +49,7 @@ case $action in
     ;;
 
   *)
-    echo "Unknown option: $action" 1>&2
-    exit 1
+    $action $THE_ARGS
     ;;
 
 esac
