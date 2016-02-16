16a5f437cabd593cbc1864b8501892a58f3f39f6
da99 <i-hate-spam-1234567@mailinator.com>
2016-02-16 13:51:18 -0600

    Touchup: watching files

--- bin.template.sh
+++ bin.template.sh
@@ -13,50 +13,50 @@ fi
 
 set -u -e -o pipefail
 
-watch () {
-  cmd () {
-    if [[ -z "$@" ]]; then
-      path="some.file"
-    else
-      path="$1"
-      shift
-    fi
-  }
-  cmd
-
-  echo -e "\n=== Watching: $files"
-  while read -r CHANGE; do
-    dir=$(echo "$CHANGE" | cut -d' ' -f 1)
-    path="${dir}$(echo "$CHANGE" | cut -d' ' -f 3)"
-    file="$(basename $path)"
-
-    # Make sure this is not a temp/swap file:
-    { [[ ! -f "$path" ]] && continue; } || :
-
-    # Check if file has changed:
-    if bash_setup is_same_file "$path"; then
-      echo "=== No change: $CHANGE"
-      continue
-    fi
-
-    # File has changed:
-    echo -e "\n=== $CHANGE ($path)"
-
-    if [[ "$path" =~ "$0" ]]; then
-      echo "=== Reloading..."
-      break
-    fi
-
-    if [[ "$file" =~ ".some_ext" ]]; then
-      cmd $path
-    fi
-  done < <(inotifywait --quiet --monitor --event close_write some_file "$0") || exit 1
-  $0 watch $THE_ARGS
-}
-
 case $action in
   help|--help)
     bash_setup print_help $0
+    ;;
+
+  watch)
+    cmd () {
+      if [[ -z "$@" ]]; then
+        path="some.file"
+      else
+        path="$1"
+        shift
+      fi
+    }
+    cmd
+
+    echo -e "\n=== Watching: $files"
+    while read -r CHANGE; do
+      dir=$(echo "$CHANGE" | cut -d' ' -f 1)
+      path="${dir}$(echo "$CHANGE" | cut -d' ' -f 3)"
+      file="$(basename $path)"
+
+      # Make sure this is not a temp/swap file:
+      { [[ ! -f "$path" ]] && continue; } || :
+
+      # Check if file has changed:
+      if bash_setup is_same_file "$path"; then
+        echo "=== No change: $CHANGE"
+        continue
+      fi
+
+      # File has changed:
+      echo -e "\n=== $CHANGE ($path)"
+
+      if [[ "$path" =~ "$0" ]]; then
+        echo "=== Reloading..."
+        break
+      fi
+
+      if [[ "$file" =~ ".some_ext" ]]; then
+        cmd $path
+      fi
+    done < <(inotifywait --quiet --monitor --event close_write some_file "$0") || exit 1
+    $0 watch $THE_ARGS
     ;;
 
   *)
