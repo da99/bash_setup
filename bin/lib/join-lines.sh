2eb2373cc4c9361a81cfff4e3be75b0855790b3f
da99 <i-hate-spam-1234567@mailinator.com>
2016-02-29 04:19:02 -0600

    Added: join-lines, bin-lib-name-conflicts

---
+++ bin/lib/join-lines.sh
@@ -0,0 +1,40 @@
+
+# === Join multiple lines into one:
+# === {{CMD}}  "content"  "delimiter"
+join-lines () {
+
+  content="$1"; shift
+  delim="$1"; shift
+  while [[ -n "$@" ]]; do
+    content="$content\n$delim"
+    delim="$1"; shift
+  done
+  new_content=""
+  total="$(echo -e "$content" | wc -l)"
+  counter=0
+
+  while IFS= read LINE; do
+    trim="$(echo -n $LINE)"
+    [[ -z "$trim" ]] && continue || :
+
+    if [[ -n "$new_content" ]]; then
+      new_content="${new_content}${delim}"
+    fi
+    new_content="${new_content}$LINE"
+  done < <(echo -e "$content")
+  echo -e "$new_content"
+
+} # === end function
+
+# Specs ===
+# bash_setup join-lines  "a\nb" ", "
+# "a, b"
+
+# bash_setup join-lines  "a\nb\n" ", "
+# "a, b"
+
+# bash_setup join-lines  "a\n" ", "
+# "a"
+
+# bash_setup join-lines  "a" ", "
+# "a"
