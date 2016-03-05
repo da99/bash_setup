86bff60530ac0855a70fe1a6fd44b8aeafa384a1
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-05 04:26:20 -0600

    Added: run all specs in dir

--- bin/lib/run-specs.sh
+++ bin/lib/run-specs.sh
@@ -1,6 +1,25 @@
 
 # === {{CMD}}  spec-run  my-shell-script.sh
 run-specs () {
+  if [[ -z "$@" ]]; then
+    while read FILE; do
+      bash_setup BOLD "=== Specs: {{$FILE}}"
+      bash_setup run-specs "$FILE" || bash_setup RED "=== {{Failed}}"
+      echo ""
+    done < <(grep -r -l -P '^specs\s*\(\)' bin/lib)
+
+
+    files="$(grep -r -l -v -P -z '^specs\s*\(\)' bin/lib || :)"
+    if [[ -n "$files" ]]; then
+      bash_setup ORANGE "=== Files {{without}} specs:"
+      echo -e "$files"
+      echo ""
+    fi
+
+    bash_setup GREEN "==== All specs {{passed}}."
+    return 0
+  fi
+
   local FILE
   FILE="$1"; shift
   source "$FILE"
