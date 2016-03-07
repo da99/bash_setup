d11bc42b0cbf504f2d9930375c5133984a76dce9
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-06 22:34:05 -0600

    Added: should-exit, should-pass

--- bin/lib/run-specs.sh
+++ bin/lib/run-specs.sh
@@ -27,11 +27,55 @@ run-specs () {
   else
     FILE="$(find bin/lib -type f -iname "$FILE_ARG")"
   fi
+
   source "$FILE"
-  specs || {
-    stat=$?;
-    bash_setup RED "=== Spec {{Failed}} with exit status: $stat";
-    exit $stat;
-  }
+
+  # NOTE: We can't use 'specs || report-fail'
+  #  because of this:
+  #  http://stackoverflow.com/questions/4072984/set-e-in-a-function
+  trap 'report-fail $?' EXIT
+  specs
+  trap - EXIT
+
 }
+
+report-fail () {
+  stat=$1; shift
+  if [[ "$stat" -ne "0" ]]; then
+    bash_setup RED "=== Spec {{Failed}} with exit status: $stat"
+  fi
+  exit $stat
+}
+
+should-pass () {
+  local stat
+  set +e
+  eval "$@"
+  stat=$?
+  set -e
+
+  if [[ "$stat" -eq "0" ]]; then
+    bash_setup GREEN  "=== {{Passed}}: $@"
+  else
+    bash_setup RED "=== {{Failed}}: exit $stat: $@"
+  fi
+}
+
+should-exit () {
+  local expect="$1"; shift
+  local actual
+  local cmd="$@"
+
+  set +e
+  eval "$@"
+  actual="$?"
+  set -e
+
+  if [[ "$expect" -eq "$actual" ]]; then
+    bash_setup GREEN  "=== {{Passed}}: exit $expect: $cmd"
+  else
+    bash_setup RED "=== Spec wanted status $expect but got {{$actual}}"
+  fi
+}
+
 
