79483bf2a78a709cbdb42dd6a63b3098aadb2d32
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-10 03:09:37 -0500

    Changed: bash_setup -> my_bash

--- bin/public/files-must-be-equal/_.sh
+++ bin/public/files-must-be-equal/_.sh
@@ -4,8 +4,8 @@ files-must-be-equal () {
   NAME="$1"; shift
   FILE1="$1"; shift
   FILE2="$1"; shift
-  # if [[ -s "$FILE1" ]] && ! diff <(my_bash trim-file "$FILE1") <(bash_setup trim-file "$FILE2"); then
-  if [[ -s "$FILE1" ]] && ! diff <(my_bash trim-file "$FILE1") <(bash_setup trim-file "$FILE2"); then
+  # if [[ -s "$FILE1" ]] && ! diff <(my_bash trim-file "$FILE1") <(my_bash trim-file "$FILE2"); then
+  if [[ -s "$FILE1" ]] && ! diff <(my_bash trim-file "$FILE1") <(my_bash trim-file "$FILE2"); then
     sh_color RED "=== $NAME {{Failed}}:"
     sh_color BOLD "{{      === Actual: =====}} $FILE2"
     cat "$FILE2"
